f5b89b8da5e9004ba16034c369ec2317f7ce0c62
bmf <bmf.geekinside@gmail.com>
2020-02-16 16:12:46 +0100

    experimentation with react hook builder

--- src/bhistory.ts
+++ src/bhistory.ts
@@ -1,14 +1,51 @@
-import { BHistoryState, UnsubscribeFunction, BHistory } from './types';
+import { useMemo, useCallback, AnchorHTMLAttributes } from 'react';
 
-export const createHistory = (): BHistory => {
+function joinAbsolutePath(...fragments: string[]): string {
+  const relativePath = fragments
+    .map(f => f.trim())
+    .filter(f => f.length > 0)
+    .map(f => {
+      let cleanedFragment = f;
+      if (f.startsWith('/')) {
+        cleanedFragment = f.slice(1);
+      }
+      if (f.endsWith('/')) {
+        cleanedFragment = f.slice(0, -1);
+      }
+      return cleanedFragment;
+    })
+    .join('/');
+
+  return `/${relativePath}`;
+}
+
+function navigate(to: string, replace: boolean): void {
+  if (!replace) {
+    window.history.pushState(null, '', to);
+  } else {
+    window.history.replaceState(null, '', to);
+  }
+}
+
+interface UseLinkHookResult {
+  href: AnchorHTMLAttributes<HTMLAnchorElement>['href'];
+  onClick: AnchorHTMLAttributes<HTMLAnchorElement>['onClick'];
+}
+
+interface UseLinkHookOptions {
+  replace?: boolean;
+}
+
+function createBrowserHistoryBundle(baseUrl: string) {
   return {
-    getState(): BHistoryState {
-      return null as any;
-    },
-    subscribe(): UnsubscribeFunction {
-      return (): void => {
-        return;
-      };
+    useLink(
+      to: string,
+      { replace = false }: UseLinkHookOptions = {}
+    ): UseLinkHookResult {
+      const href = useMemo(() => joinAbsolutePath(baseUrl, to), [to]);
+      const onClick = useCallback(() => navigate(to, replace), [to, replace]);
+
+      return useMemo(() => ({ href, onClick }), [href, onClick]);
     },
   };
-};
+}
