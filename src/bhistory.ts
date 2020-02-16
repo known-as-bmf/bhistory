1af32846ada880f487afb2732c5ca883fc5983ad
bmf <bmf.geekinside@gmail.com>
2020-02-16 22:56:54 +0100

    wip

--- src/bhistory.ts
+++ src/bhistory.ts
@@ -36,6 +36,60 @@ interface UseLinkHookOptions {
   replace?: boolean;
 }
 
+interface NavigateOptions {
+  replace?: boolean;
+}
+
+interface BHistory {
+  navigate(to: string, options: NavigateOptions): void;
+}
+
+interface BrowserHistoryOptions {
+  basePath?: string;
+  useHash?: boolean;
+}
+
+export function createBrowserHistory({
+  basePath = '/',
+  useHash = false,
+}: BrowserHistoryOptions = {}): BHistory {
+  function popstateEventHandler() {
+    /** noop for now */
+  }
+
+  window.addEventListener('popstate', popstateEventHandler);
+
+  return {
+    navigate(to: string, { replace = false }: NavigateOptions = {}): void {
+      if (!replace) {
+        window.history.pushState(
+          null,
+          '',
+          `${useHash ? '#' : ''}${basePath}${to}`
+        );
+      } else {
+        window.history.replaceState(
+          null,
+          '',
+          `${useHash ? '#' : ''}${basePath}${to}`
+        );
+      }
+    },
+  };
+}
+
+function createMemoryHistory(): BHistory {
+  return {
+    navigate(to: string, { replace = false }: NavigateOptions = {}): void {
+      if (!replace) {
+        window.history.pushState(null, '', to);
+      } else {
+        window.history.replaceState(null, '', to);
+      }
+    },
+  };
+}
+
 function createBrowserHistoryBundle(baseUrl: string) {
   return {
     useLink(
